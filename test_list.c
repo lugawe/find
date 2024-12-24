7a3390315f969c05b75bba300abc80d8189e3aa6
Luis Weich <luis.weich@study.thws.de>
2024-12-24 17:48:42 +0100

    add test_list.c

---
+++ test_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+void print_int(void *content) { printf("%d\n", *(int *)content); }
+
+int main() {
+    List *list = list_create();
+    int a = 2;
+    int b = 3;
+    int c = 5;
+    int d = -42;
+    list_add(list, &a);
+    list_add(list, &b);
+    list_add(list, &c);
+    list_add(list, &d);
+    list_traverse(list, print_int);
+    list_free(list);
+    return 0;
+}
+
