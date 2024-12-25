57a70e0ab335247e0ab944f18e430cae03dd10e1
Luis Weich <luis.weich@study.thws.de>
2024-12-25 19:10:51 +0100

    main.c prints all files in working directory

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "files.h"
 
 int main(int argc, char **argv) {
-    printf("Stimme zu\n");
+    // todo parse arguments
+
+    int size = 0;
+    File *files = list_files(".", &size);
+
+    for (int i = 0; i < size; i++) {
+        print_file(&files[i]);
+    }
+
     return 0;
 }
 
