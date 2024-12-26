beded56f91dbf33adde715b151db16962ddfb56e
Luis Weich <luis.weich@study.thws.de>
2024-12-26 13:18:55 +0100

    main update

--- main.c
+++ main.c
@@ -3,14 +3,22 @@
 
 #include "files.h"
 
-int main(int argc, char **argv) {
-    // todo parse arguments
-
+void print_files(char *dir) {
     int size = 0;
-    File *files = list_files(".", &size);
+    File *files = list_files(dir, &size);
 
     for (int i = 0; i < size; i++) {
         print_file(&files[i]);
+    }
+}
+
+int main(int argc, char **argv) {
+    // todo parse arguments
+
+    if (argc == 1) {
+        print_files(".");
+    } else if (argc == 2) {
+        print_files(argv[1]);
     }
 
     return 0;
