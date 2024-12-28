6f408554ff54818e289393ec6955108ad364ef1f
Luis Weich <luis.weich@study.thws.de>
2024-12-28 18:32:01 +0100

    main.c naming updates

--- main.c
+++ main.c
@@ -5,7 +5,12 @@
 
 void print_file(File *file) { printf("%s\n", file->path); }
 
-void print_files(char *dir) {
+int file_truepredicate(File *file) {
+    print_file(file);
+    return 1;
+}
+
+void list_print_files(char *dir) {
     int size = 0;
     File *files = list_files_rec(dir, 1000, &size);  // todo to be discussed
 
@@ -14,13 +19,17 @@ void print_files(char *dir) {
     }
 }
 
+void traverse_print_files(char *dir) {
+    traverse_files_rec(dir, 1000, file_truepredicate);  // todo to be discussed
+}
+
 int main(int argc, char **argv) {
     // todo parse arguments
 
     if (argc == 1) {
-        print_files(".");
+        list_print_files(".");
     } else if (argc == 2) {
-        print_files(argv[1]);
+        list_print_files(argv[1]);
     }
 
     return 0;
